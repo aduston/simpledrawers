761de4b3fde3bc3d2b8ed6cd0ca12b55bc41847b
Adam Duston <aduston@gmail.com>
2016-03-20 14:55:00 -0400

    Finished first version of drawers

--- drawers.py
+++ drawers.py
@@ -1,11 +1,49 @@
+from OCC import gp, TopLoc
 from OCC.Display.SimpleGui import init_display
+from OCC.TopoDS import TopoDS_Builder, TopoDS_Compound
 from drawer import make_drawer
-from drawer_box import make_drawer_box
+from drawer_box import make_drawer_box, BACK_INSET
+from constants import THICKNESS_0, THICKNESS_1
+
+def _move(shape, x, y, z):
+    tr = gp.gp_Trsf()
+    tr.SetTranslation(gp.gp_Vec(x, y, z))
+    loc = TopLoc.TopLoc_Location(tr)
+    shape.Move(loc)
+
+def make_drawers(dx, dy, dz, arrangement):
+    available_z_space = dz - THICKNESS_0 * (len(arrangement) + 1)
+    drawer_height = available_z_space / len(arrangement)
+    drawer_depth = dy - BACK_INSET - THICKNESS_1
+    offsets = []
+    for i in range(len(arrangement) - 1):
+        offsets.append(THICKNESS_0 + (i + 1) * (drawer_height + THICKNESS_0))
+    drawer_box = make_drawer_box(dx, dy, dz, offsets)
+    drawers = []
+    for level, num_drawers in enumerate(arrangement):
+        drawer_width = (dx - THICKNESS_0 * 2) / float(num_drawers)
+        z_pos = dz - (level + 1) * (THICKNESS_0 + drawer_height)
+        for drawer_index in range(num_drawers):
+            drawer = make_drawer(drawer_width, drawer_depth, drawer_height)
+            _move(drawer, THICKNESS_0 + drawer_width * drawer_index, 0, z_pos)
+            drawers.append(drawer)
+
+    builder = TopoDS_Builder()
+    compound = TopoDS_Compound()
+    builder.MakeCompound(compound)
+    builder.Add(compound, drawer_box)
+    for drawer in drawers:
+        builder.Add(compound, drawer)
+    return compound
+
+paper_size = (11 * 2.54, 8.5 * 2.54)
+drawers = make_drawers(
+    paper_size[0] + 2 + THICKNESS_0 * 2,
+    paper_size[1] + 2 + THICKNESS_0 * 2 + THICKNESS_1 + BACK_INSET,
+    26,
+    [3, 1, 1])
 
 display, start_display, add_menu, add_function_to_menu = init_display()
-# drawer = make_drawer(20, 30, 10)
-drawer_box = make_drawer_box(30, 25, 40, [10, 20])
-
-display.DisplayShape(drawer_box, update=True)
+display.DisplayShape(drawers, update=True)
 start_display()
 
