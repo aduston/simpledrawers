44b2ae72edc06093a56b5eaf999a361fd7e4f1ff
Adam Duston <aduston@gmail.com>
2016-03-20 11:49:24 -0400

    This should complete the individual drawer model

--- drawer.py
+++ drawer.py
@@ -1,7 +1,9 @@
+import math
 from OCC import gp, TopLoc
 from OCC.gp import gp_Pnt
-from OCC.BRepPrimAPI import BRepPrimAPI_MakeBox
+from OCC.BRepPrimAPI import BRepPrimAPI_MakeBox, BRepPrimAPI_MakeCylinder
 from OCC.BRepAlgoAPI import BRepAlgoAPI_Cut
+from OCC.BRepBuilderAPI import BRepBuilderAPI_Transform
 from OCC.TopoDS import TopoDS_Builder, TopoDS_Compound
 from constants import THICKNESS_0, THICKNESS_1
 
@@ -15,34 +17,52 @@ _cut = lambda *args: BRepAlgoAPI_Cut(*args).Shape()
 _pnt = gp_Pnt
 
 def make_drawer(dx, dy, dz):
-    side_0 = _make_side(dy, dz, True)
-    side_1 = _make_side(dy, dz, False)
-    _move(side_1, dx - THICKNESS_0, 0, 0)
-
-    front = _make_end(dx, dz, True)
-    back = _make_end(dx, dz, False)
-    _move(back, 0, dy - THICKNESS_0, 0)
+    pieces = [
+        _make_side(dx, dy, dz, True),
+        _make_side(dx, dy, dz, False),
+        _make_end(dx, dy, dz, True),
+        _make_end(dx, dy, dz, False),
+        _make_bottom(dx, dy)]
 
     builder = TopoDS_Builder()
     compound = TopoDS_Compound()
     builder.MakeCompound(compound)
-    builder.Add(compound, side_0)
-    builder.Add(compound, side_1)
-    builder.Add(compound, front)
-    builder.Add(compound, back)
+    for piece in pieces:
+        builder.Add(compound, piece)
     return compound
-    
-    # front = _make_front(dx, dz)
-    # back = _make_front(dx, dz)
-    # bottom = _make_bottom(dx, dy)
-    # return _assemble_drawer(side_0, side_1, front, back, bottom, dx,
-    # dy)
 
-def _make_end(dx, dz, is_front):
+def _make_bottom(dx, dy):
+    bottom = _box(dx - THICKNESS_0 * 2 + NOTCH_DEPTH * 2,
+                  dy - THICKNESS_0 * 2 + NOTCH_DEPTH * 2,
+                  THICKNESS_1)
+    _move(bottom, THICKNESS_0 - NOTCH_DEPTH, THICKNESS_0 - NOTCH_DEPTH, BOTTOM_INSET)
+    return bottom
+
+def _make_end(dx, dy, dz, is_front):
     end = _box(dx, THICKNESS_0, dz)
     end = _cut_end_fingers(end, 0, dz)
     end = _cut_end_fingers(end, dx - THICKNESS_0, dz)
-    return _cut_end_bottom_notch(end, dx, is_front)
+    end = _cut_end_bottom_notch(end, dx, is_front)
+    if is_front:
+        end = _cut_finger_notch(end, dx, dz)
+    if not is_front:
+        _move(end, 0, dy - THICKNESS_0, 0)
+    return end
+
+def _cut_finger_notch(front, dx, dz):
+    finger_width = 2.0
+    finger_height = 1.0
+    front = _cut(front, _box(_pnt(dx / 2. - finger_width / 2., 0, dz - finger_height),
+                             finger_width, THICKNESS_0, finger_height))
+    cyl = BRepPrimAPI_MakeCylinder(finger_width / 2.0, THICKNESS_0).Shape()
+    tr = gp.gp_Trsf()
+    tr.SetRotation(gp.gp_Ax1(gp.gp_Pnt(0, 0, 0), gp.gp_Dir(1, 0, 0)), math.pi / 2.0)
+    tr2 = gp.gp_Trsf()
+    tr2.SetTranslation(gp.gp_Vec(dx / 2., THICKNESS_0, dz - finger_height))
+    tr2.Multiply(tr)
+    cyl = BRepBuilderAPI_Transform(cyl, tr2, True).Shape()
+    front = _cut(front, cyl)
+    return front
 
 def _cut_end_fingers(end, x, dz):
     offset = BOTTOM_SPACE
@@ -57,11 +77,14 @@ def _move(shape, x, y, z):
     loc = TopLoc.TopLoc_Location(tr)
     shape.Move(loc)
     
-def _make_side(dy, dz, is_left_side):
+def _make_side(dx, dy, dz, is_left_side):
     side = _box(THICKNESS_0, dy, dz)
     side = _cut_side_fingers(side, 0, dz)
     side = _cut_side_fingers(side, dy - THICKNESS_0, dz)
-    return _cut_side_bottom_notch(side, dy, is_left_side)
+    side = _cut_side_bottom_notch(side, dy, is_left_side)
+    if not is_left_side:
+        _move(side, dx - THICKNESS_0, 0, 0)
+    return side
 
 def _cut_side_fingers(side, y, dz):
     side = _cut(side, _box(_pnt(0, y, 0),
